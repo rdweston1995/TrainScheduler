91721de0f65bf839c302a3825d6ec5cbe6c4fd75
Robert <robert.d.weston@gmail.com>
2019-05-09 13:10:59 -0700

    Init

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDCVGHIxBAf6Ba2h0Lm8KM9on0c1V2-xE4",
+    authDomain: "train-scheduler-1a25b.firebaseapp.com",
+    databaseURL: "https://train-scheduler-1a25b.firebaseio.com",
+    projectId: "train-scheduler-1a25b",
+    storageBucket: "train-scheduler-1a25b.appspot.com",
+    messagingSenderId: "72342945746",
+    appId: "1:72342945746:web:f41442553a0bfe23"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
