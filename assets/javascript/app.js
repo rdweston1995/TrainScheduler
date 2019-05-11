30078168ad401ac1901e80ef41bd766c960055f8
Robert <robert.d.weston@gmail.com>
2019-05-10 20:22:32 -0700

    Finished javascript with basics of the assignment done. Only thing left is the styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,7 +17,8 @@ $(document).ready(function(){
   var trainCount = 0;
   
   $("#submit").on("click", function(){
-    var trainName = $("#trainName").val().trim();
+    
+    var trainName = $("#addTrain").val().trim();
     var destination = $("#addDestination").val().trim();
     var trainTime = $("#firstTrainTime").val().trim();
     var frequency = $("#frequency").val().trim();
@@ -30,12 +31,71 @@ $(document).ready(function(){
     };
 
     database.ref().push(newTrain);
+
+    $("#addTrain").val("");
+    $("#addDestination").val("");
+    $("#firstTrainTime").val("");
+    $("#frequency").val("");
   });
 
-  database.ref().on("clild_added", function(childSnapshot){
-    console.log(childSnapshot);
-    $(".newTrains").append("<tr class='train" + trainCount + "'><td>" + childSnapshot.val().trainName + "</td><td>" +
-        childSnapshot.val().destination + "</td><td>" + childSnapshot.val().trainTime + 
-        "</td><td>" + childSnapshot.val().frequency + "</td></tr>");
+  
+  database.ref().on("child_added", function(childSnapshot){
+    //console.log(childSnapshot);
+    var hourStart = childSnapshot.val().trainTime;
+    hourStart = parseInt(hourStart.substr(0,2));
+    var minStart = childSnapshot.val().trainTime;
+    minStart = parseInt(minStart.substr(minStart.length - 2, minStart.length - 1));
+    var minTimeStart = (hourStart * 60) + minStart;
+
+    var hourCurrent = moment().hour();
+    var minCurrent = moment().minute();
+    var minTimeCurrent = (hourCurrent * 60) + minCurrent;
+
+    var timePastStart = minTimeCurrent - minTimeStart;
+    var totalRuns = Math.floor(timePastStart / parseInt(childSnapshot.val().frequency));
+    var timeTillNext = parseInt(childSnapshot.val().frequency) - (timePastStart % parseInt(childSnapshot.val().frequency));
+
+    var nextHour = hourCurrent;
+    var nextMin = minCurrent + timeTillNext;
+    if(nextMin >= 60){
+      nextMin -= 60;
+      nextHour++;
+    }
+
+    if(nextHour >= 24){
+      nextHour = 0;
+    }
+
+    if(nextMin === 0){
+      nextMin = "00";
+    }
+    
+    var amPM = "AM";
+
+    if(nextHour >= 12){
+      amPM = "PM";
+    }
+    
+    //console.log("---------------");
+
+    nextHour = nextHour % 12;
+    if(nextHour === 0){
+      nextHour = 12;
+    }
+
+    
+    
+
+    //Adding each child in the database to the table
+    $(".trainsTable").append("<tr class='train" + trainCount + "'><td scope='col'>" + childSnapshot.val().trainName + 
+        "</td><td scope='col'>" + childSnapshot.val().destination + 
+        "</td><td scope='col'>" + childSnapshot.val().frequency + 
+        "</td><td scope='col'>" + nextHour + ":" + nextMin + " " + amPM +
+        "</td><td scope='col'>" + timeTillNext + "</td></tr>");
+    
+    //Adding one to the trainCount variable    
+    trainCount++;
   });
+
+
 });
